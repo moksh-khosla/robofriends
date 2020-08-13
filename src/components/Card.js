582cbc006bd92c78fbc66ab99c87d5152d9f50d0
Moksh Khosla <moksh.khosla33@gmail.com>
2020-08-13 14:13:00 +0530

    Deploy Ready build

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,7 +8,6 @@ const Card = (props) => {
             <div>
                 <h2>{name}</h2>
                 <p>{email}</p>
-                <p>{address.city}</p>
             </div>
         </div>
     );
